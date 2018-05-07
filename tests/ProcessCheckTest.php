ae782af4863ed458bdb9f2e967fb3b9492c308db
mozartk-mac <mozartk30@gmail.com>
2018-05-07 12:37:32 +0900

    refactoring tests

--- tests/ProcessCheckTest.php
+++ tests/ProcessCheckTest.php
@@ -3,50 +3,82 @@
 namespace mozartk\processCheck\Test;
 use mozartk\processCheck\ProcessCheck;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
 
 class ProcessCheckTest extends TestCase
 {
 
     public function testSetConfigPath()
     {
+        $this->expectException('mozartk\processCheck\Exception\LoadConfigException');
+
         $testPath = "/config/file/not/exists";
         $process = new ProcessCheck();
         $process->setConfigPath($testPath);
         $this->assertEquals($testPath, $process->getConfigPath());
     }
+    /*
+        public function testCantReadableConfig()
+        {
+            $this->expectException('mozartk\processCheck\Exception\LoadConfigException');
 
-    public function testSetConfigPathSpace()
+            $testPath = "tests/config.imp.json";
+            $process = new ProcessCheck();
+            $process->setConfigPath($testPath);
+            $this->assertEquals($testPath, $process->getConfigPath());
+
+        }
+    */
+    public function testResultClassDoesNotExists()
     {
+        $this->expectException('mozartk\processCheck\Exception\NotExistsParserResultException');
+
+        $testPath = "tests/config.wrongclass.json";
         $process = new ProcessCheck();
-
-        $process->setConfigPath(" ");
-        $this->assertEquals(ProcessCheck::BASIC_CONFIGPATH, $process->getConfigPath());
-    }
-
-    public function testSetConfigPathExists()
-    {
-        $process = new ProcessCheck();
-
-        $process->setConfigPath("./config.json");
-        $this->assertEquals(ProcessCheck::BASIC_CONFIGPATH, $process->getConfigPath());
-    }
-
-    public function testRunNotExistsConfig()
-    {
-        $this->expectException('mozartk\processCheck\Exception\LoadConfigException');
-
-        $process = new ProcessCheck();
-        $process->setConfigPath("/mozartk/notExistsFiles");
+        $process->setConfigPath($testPath);
         $process->run();
     }
 
-    public function testCheckProcessValidJson()
+    public function testResultJson()
     {
+        $testPath = "tests/config.json";
         $process = new ProcessCheck();
-        $process->setConfigPath("./tests/config.json");
+        $process->setConfigPath($testPath);
         $result = $process->run();
 
-        $jsonResult = json_decode($result);
+        json_decode($result);
         $this->assertEquals(json_last_error(),JSON_ERROR_NONE);
+    }
+
+    public function testResultYaml()
+    {
+        $testPath = "tests/config.yaml.json";
+        $process = new ProcessCheck();
+        $process->setConfigPath($testPath);
+        $result = $process->run();
+
+        $res_arr = Yaml::parse($result);
+
+        $this->assertTrue(is_array($res_arr));
+    }
+
+    public function testResultIni()
+    {
+        $testPath = "tests/config.ini.json";
+        $process = new ProcessCheck();
+        $process->setConfigPath($testPath);
+        $result = $process->run();
+
+        $this->assertTrue(is_array(parse_ini_string($result)));
+    }
+
+    public function testResultClassNotExists()
+    {
+        $this->expectException("mozartk\processCheck\Exception\NotExistsParserResultException");
+
+        $testPath = "tests/config.wrongclass.json";
+        $process = new ProcessCheck();
+        $process->setConfigPath($testPath);
+        $result = $process->run();
     }
 }
