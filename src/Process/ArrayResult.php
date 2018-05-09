59875d08cc0263da73ffe776c42859f8406df418
mozartk-mac <mozartk30@gmail.com>
2018-05-09 20:43:41 +0900

    add php native array result

---
+++ src/Process/ArrayResult.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace mozartk\ProcessChecker\Process;
+
+class ArrayResult implements ResultInterface
+{
+    private $parseData = array();
+    public function parse($processName, $data = null)
+    {
+        $jsonData = array();
+        $this->parseData[$processName] = array();
+        foreach ($data as $process) {
+            $jsonData['name'] = $process->getName();
+            $jsonData['name_w'] = $process->getWindowTitle();
+            $jsonData['cputime'] = $process->getCpuTime();
+            $jsonData['pid'] = $process->getPid();
+            $jsonData['running'] = (boolean) $process->isRunning();
+
+            $this->parseData[$processName][] = $jsonData;
+        }
+    }
+
+    public function clear()
+    {
+        $this->parseData = array();
+    }
+
+    public function get()
+    {
+        return $this->parseData;
+    }
+}
